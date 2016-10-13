685770ccedf6347cea69a7092a3faa9434867373
dimhold <dimhold@gmail.com>
2016-10-13 12:38:37 +0300

    Update user usage

--- src/metrics/increaseUserEntropy.js
+++ src/metrics/increaseUserEntropy.js
@@ -3,7 +3,7 @@ var logger = require("../log/logger");
 module.exports = {
   name: module.id.match(/^.*\/([^\\\/]+).js$/)[1],
   calculate (randoChooser, randoToMark, randos) { 
-    var chooserUser = getUserByEmail(randoChooser.email);
+    var chooserUser = global.users[randoChooser.email];
     if (chooserUser && userToMark) {
       if (containsEmail(randoToMark.email, chooserUser.in)) {
         logger.trace("[increaseUserEntropy] -5 for rando chooser:", randoChooser.randoId, "and randoToMark:", randoToMark.randoId);
@@ -14,17 +14,6 @@ module.exports = {
     return 0;
   }
 };
-
-function getUserByEmail (email) {
-  for (var i = 0; i < global.users.length; i++) {
-    if (global.users[i].email === email) {
-      logger.trace("[increaseUserEntropy] user found:", global.users[i].email);
-      return global.users[i];
-    }
-  }
-  logger.trace("[increaseUserEntropy] user NOT found", email);
-  return null;
-}
 
 function containsEmail (email, randos) {
   for (var i = 0; i < randos.length; i++) {
