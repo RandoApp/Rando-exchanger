5453087a42f748463ac5e6d46b3075caa910ad1d
dimhold <dimhold@gmail.com>
2016-10-20 18:13:54 +0300

    Fix metric

--- src/metrics/increaseUserEntropy.js
+++ src/metrics/increaseUserEntropy.js
@@ -5,20 +5,36 @@ module.exports = {
   calculate (randoChooser, randoToMark, randos) { 
     var chooserUser = global.users[randoChooser.email];
     if (chooserUser) {
-      if (containsEmail(randoToMark.email, chooserUser.in)) {
-        logger.trace("[increaseUserEntropy] -5 for rando chooser:", randoChooser.randoId, "and randoToMark:", randoToMark.randoId);
-        return -5;
-      }
+      return getMarkBasedOnLast10Randos(randoToMark.email, chooserUser.in);
     }
     return 0;
   }
 };
 
-function containsEmail (email, randos) {
-  for (var i = 0; i < randos.length; i++) {
+function getMarkBasedOnLast10Randos (email, randos) {
+  sortRandosByCreation(randos);
+
+  for (var i = 0; i < randos.length ; i++) {
+    if (i > 10) {
+      break;
+    }
+
     if (randos[i].email === email) {
-      return true;
+      return i * 10 - 100;
     }
   }
-  return false;
+
+  return 0;
+}
+
+function sortRandosByCreation(randos) {
+  return randos.sort(function (rando1, rando2) {
+    if (rando1.creation < rando2.creation) {
+      return -1;
+    } else if (rando1.creation > rando2.creation) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 }
