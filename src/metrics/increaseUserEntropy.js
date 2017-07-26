99d649f0e98bb088637be5b1a3b21b692086c092
dimhold <dimhold@gmail.com>
2017-07-26 15:34:19 +0300

    Fix codacy comments

--- src/metrics/increaseUserEntropy.js
+++ src/metrics/increaseUserEntropy.js
@@ -1,12 +1,12 @@
-var logger = require("../log/logger");
-var config = require("config");
+const logger = require("../log/logger");
+const config = require("config");
 
 module.exports = {
   name: module.id.match(/^.*\/([^\\\/]+).js$/)[1],
-  calculate (randoChooser, randoToMark, randos) { 
+  calculate (randoChooser, randoToMark, randos) {
     var chooserUser = global.users[randoChooser.email];
     if (chooserUser && Array.isArray(chooserUser.in)) {
-      var alreadyExchangedWithThisUser = chooserUser.in.filter(rando => {return rando.email === randoToMark.email}).length > 0;
+      var alreadyExchangedWithThisUser = chooserUser.in.filter(rando => rando.email === randoToMark.email).length > 0;
       if (alreadyExchangedWithThisUser) {
         return config.app.metrics.increaseUserEntropy;
       }
--- src/metrics/preventPairing.js
+++ src/metrics/preventPairing.js
@@ -1,10 +1,10 @@
-var logger = require("../log/logger");
-var config = require("config");
-var dbService = require("../service/dbService")
+const logger = require("../log/logger");
+const config = require("config");
+const dbService = require("../service/dbService");
 
 module.exports = {
   name: module.id.match(/^.*\/([^\\\/]+).js$/)[1],
-  calculate (randoChooser, randoToMark, randos) { 
+  calculate (randoChooser, randoToMark, randos) {
     var strangerEmail = getStrangerEmailByStrangerRandoId(randoChooser.strangerRandoId, randos);
     if (strangerEmail === randoToMark.email) {
       return config.app.metrics.MIN_MARK;
@@ -13,9 +13,9 @@ module.exports = {
   }
 };
 
-function getStrangerEmailByStrangerRandoId(strangerRandoId, randos) {
+function getStrangerEmailByStrangerRandoId (strangerRandoId, randos) {
   var strangerEmail = "";
-  var strangers = global.strangerEmailsAndRandoIds.filter(stranger => {return stranger.randoId === strangerRandoId});
+  var strangers = global.strangerEmailsAndRandoIds.filter(stranger => stranger.randoId === strangerRandoId);
   if (strangers.length > 0) {
     strangerEmail = strangers[0].email;
   }
